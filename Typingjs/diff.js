43ed3de965aa4975ab3360a930d1701dccf810a7
yogeshKulkarni20 <thisisyogeshkulkarni@gmail.com>
2024-03-05 14:49:29 +0530

    fix(lcs-algo): updated logic to handle words only

--- Typingjs/diff.js
+++ Typingjs/diff.js
@@ -7,8 +7,11 @@
  * @returns string
  */
 const getLongestContinuousSequence = (expectedPassage, userInput) => {
-  const m = expectedPassage.length;
-  const n = userInput.length;
+  const expectedPassageWords = expectedPassage.trim().split(" ");
+  const userInputWords = userInput.trim().split(" ");
+
+  const m = expectedPassageWords.length;
+  const n = userInputWords.length;
 
   // Create a 2D array to store the lengths of LCS
   const dp = [];
@@ -19,7 +22,7 @@ const getLongestContinuousSequence = (expectedPassage, userInput) => {
   // Build the dp array in bottom-up manner
   for (let i = 1; i <= m; i++) {
     for (let j = 1; j <= n; j++) {
-      if (expectedPassage[i - 1] === userInput[j - 1]) {
+      if (expectedPassageWords[i - 1] === userInputWords[j - 1]) {
         dp[i][j] = dp[i - 1][j - 1] + 1;
       } else {
         dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
@@ -27,13 +30,13 @@ const getLongestContinuousSequence = (expectedPassage, userInput) => {
     }
   }
 
-  // Reconstruct the LCS
+  // Reconstruct the correctWords
   let i = m;
   let j = n;
-  const lcs = [];
+  const correctWords = [];
   while (i > 0 && j > 0) {
-    if (expectedPassage[i - 1] === userInput[j - 1]) {
-      lcs.unshift(expectedPassage[i - 1]);
+    if (expectedPassageWords[i - 1] === userInputWords[j - 1]) {
+      correctWords.unshift(expectedPassageWords[i - 1]);
       i--;
       j--;
     } else if (dp[i - 1][j] > dp[i][j - 1]) {
@@ -43,8 +46,6 @@ const getLongestContinuousSequence = (expectedPassage, userInput) => {
     }
   }
 
-  const correctWords = lcs.join("").split(/\s+/).filter(Boolean);
-  console.log({ correctWords });
   return correctWords;
 };
 
@@ -59,19 +60,20 @@ const getCorrectWordsHighlighted = (wordsCorrectlyTyped, expectedWords) => {
     typedWordIndex < wordsCorrectlyTyped.length &&
     expectedWordsIndex < expectedWords.length
   ) {
+    const currentExpectedWord = expectedWords[expectedWordsIndex];
+    const currentCorrectWord = wordsCorrectlyTyped[typedWordIndex];
     // If the words match, move to the next word in both arrays
-    if (
-      expectedWords[expectedWordsIndex] === wordsCorrectlyTyped[typedWordIndex]
-    ) {
+    if (currentExpectedWord === currentCorrectWord) {
       const currentElement = spansStartingWithW[expectedWordsIndex];
       currentElement.style.color = "green";
-      expectedWordsIndex += 1;
       typedWordIndex += 1;
       correctWords += 1;
-    } else {
-      // If the words don't match, move to the next word in the expectedWords
-      expectedWordsIndex += 1;
+    } else if (!expectedWords.includes(currentCorrectWord)) {
+      // If the words from wordsCorrectlyTyped is not present in expectedWords, move to the next word in wordsCorrectlyTyped
+      typedWordIndex += 1;
     }
+    expectedWordsIndex += 1;
   }
+
   return correctWords;
 };
